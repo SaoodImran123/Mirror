cec8121bc6c6b68828efd0c9daebd2261337adae
EthanBautista <raffybautista07@gmail.com>
2021-11-23 21:32:50 -0500

    broken code

--- src/socketController.js
+++ src/socketController.js
@@ -1,29 +1,39 @@
-
 peers = {}
-
+room = {}
 
 module.exports = (io) => {
     io.on('connect', (socket) => {
         console.log('a client is connected')
 
-
         // Initiate the connection process as soon as the client connects
+        peers[socket.id] = socket;
 
-        peers[socket.id] = socket
+        // MR traffic guy
+        room[socket.id] = new Array();
+        room[socket.id].push(socket);
+        socket.emit('initialSocket', socket.id);
 
-        // Asking all other clients to setup the peer connection receiver
-        for(let id in peers) {
-            if(id === socket.id) continue
-            console.log('sending init receive to ' + socket.id)
-            peers[id].emit('initReceive', socket.id)
-        }
+        /**
+         * Event listener when 
+         */
+        socket.on('connectToRoom', data => {
+            console.log(room[data]);
+            console.log(data);
+            room[data].push(socket.id);
+            // connect to the room
+            for (var i = 0; i < room[data].length; i++){
+                if (room[data][i].id === socket.id) continue
+                console.log('sending init receive to ' + socket.id)
+                room[data][i].emit('initReceive', socket.id)
+            }
+        })
 
         /**
          * relay a peerconnection signal to a specific socket
          */
         socket.on('signal', data => {
             console.log('sending signal from ' + socket.id + ' to ', data)
-            if(!peers[data.socket_id])return
+            if (!peers[data.socket_id]) return
             peers[data.socket_id].emit('signal', {
                 socket_id: socket.id,
                 signal: data.signal
